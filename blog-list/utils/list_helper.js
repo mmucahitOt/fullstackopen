126dd633360f5cfeab5a3df5e798dd0e8c6a3704
mmucahitOt <m.mucahit0@gmail.com>
2025-05-22 22:10:17 +0300

    Helper Functions and Unit Tests, step 4 is implemented

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -10,8 +10,22 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0]);
 };
 
+const mostBlogs = (blogs) => {
+  const authors = blogs.map((blog) => blog.author);
+  const authorCounts = authors.reduce((acc, author) => {
+    acc[author] = (acc[author] || 0) + 1;
+    return acc;
+  }, {});
+  const mostFrequentAuthor = Object.keys(authorCounts).reduce((a, b) =>
+    authorCounts[a] > authorCounts[b] ? a : b
+  );
+  return { author: mostFrequentAuthor, blogs: authorCounts[mostFrequentAuthor] };
+};
+
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
