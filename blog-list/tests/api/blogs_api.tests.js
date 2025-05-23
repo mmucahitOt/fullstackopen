647414d25029d1605aaa5e8387379cf157a473c4
mmucahitOt <m.mucahit0@gmail.com>
2025-05-23 13:11:59 +0300

    Blog List Expansions, step 1 is implemented

--- blog-list/tests/api/blogs_api.tests.js
+++ blog-list/tests/api/blogs_api.tests.js
@@ -3,65 +3,13 @@ const { test, after, beforeEach, describe } = require("node:test");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../../app");
-const Blog = require("../../models/blog");
+const { resetDatabase, initialBlogs } = require("../helpers/seedBlogs");
 
 const api = supertest(app);
 
-const initialBlogs = [
-  {
-    _id: new mongoose.Types.ObjectId("5a422a851b54a676234d17f7"),
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: new mongoose.Types.ObjectId("5a422aa71b54a676234d17f8"),
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: new mongoose.Types.ObjectId("5a422b3a1b54a676234d17f9"),
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: new mongoose.Types.ObjectId("5a422b891b54a676234d17fa"),
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: new mongoose.Types.ObjectId("5a422ba71b54a676234d17fb"),
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: new mongoose.Types.ObjectId("5a422bc61b54a676234d17fc"),
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0,
-  },
-];
-
 describe("tests for blogs", () => {
   beforeEach(async () => {
-    await Blog.deleteMany({});
-    await Blog.insertMany(initialBlogs);
+    await resetDatabase();
   });
 
   after(async () => {
@@ -145,6 +93,18 @@ describe("tests for blogs", () => {
       };
 
       await api.post("/api/blogs").send(newBlog).expect(400);
+    });
+  });
+
+  describe("test delete /api/blogs/:id", () => {
+    test("a blog can be deleted", async () => {
+      const blogToDelete = initialBlogs[0];
+
+      const deleteResponse = await api.delete(`/api/blogs/${blogToDelete._id}`).expect(204);
+      assert.strictEqual(deleteResponse.status, 204);
+
+      const response = await api.get("/api/blogs");
+      assert.strictEqual(response.body.length, initialBlogs.length - 1, "blog was not deleted");
     });
   });
 });
