f79dab88e9d61ccc38d23c3ab7742cb18ca10af8
mmucahitOt <m.mucahit0@gmail.com>
2025-05-24 16:57:12 +0300

    Blog List Frontend, step 1 is implemented

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -11,14 +11,12 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
+  return (<div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-    </div>
-  )
+    </div>)
 }
 
 export default App
