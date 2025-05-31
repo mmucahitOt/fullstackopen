397a946e90b57c23cf67d154a1e8f0b272866475
mmucahitOt <m.mucahit0@gmail.com>
2025-05-31 09:35:07 +0300

    Blog List Tests, step 2 is implemented

--- bloglist-frontend/src/views/blog/components/blogDetail/BlogDetail.test.jsx
+++ bloglist-frontend/src/views/blog/components/blogDetail/BlogDetail.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
-import { test, expect } from 'vitest'
+import { test, expect, describe } from 'vitest'
 import BlogDetail from '../BlogDetail/BlogDetail'
+import userEvent from '@testing-library/user-event'
 
 test('does not show url and user name by default', () => {
   const blog = {
@@ -27,4 +28,31 @@ test('does not show url and user name by default', () => {
   // User name should not be visible
   const userNameElement = screen.queryByText(blog.user.name)
   expect(userNameElement).toBeNull()
+})
+
+test('shows url and user name when expanded', async () => {
+  const blog = {
+    id: '1',
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'https://test.com',
+    likes: 0,
+    user: {
+      name: 'Test User'
+    }
+  }
+
+  const user = userEvent.setup()
+
+  render(<BlogDetail blog={blog} />)
+  await user.click(screen.getByText('view'))
+
+  const titleAuthorElement = screen.getByText(`${blog.title} ${blog.author}`)
+  expect(titleAuthorElement).toBeDefined()
+
+  const urlElement = screen.getByText(blog.url)
+  expect(urlElement).toBeDefined()
+
+  const userNameElement = screen.getByText(blog.user.name)
+  expect(userNameElement).toBeDefined()
 })
