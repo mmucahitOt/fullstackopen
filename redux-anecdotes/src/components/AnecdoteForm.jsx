2834a2e86ae63b5fa743d9ead025047d74d61b58
mmucahitOt <m.mucahit0@gmail.com>
2025-06-17 20:14:24 +0300

    Anecdotes, step 1, 2, 3, 4, 5 and 6 are implemented

---
+++ redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,24 @@
+import { createEvent } from '../reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+  
+  const create = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    dispatch(createEvent(content))
+    event.target.anecdote.value = ''
+  }
+  return (
+    <div>
+      <h2>create new</h2>
+    <form onSubmit={create}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
