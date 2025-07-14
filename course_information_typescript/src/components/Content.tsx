b6ca9050d7eee0f82e583a2a0c5ea0306b42875d
mmucahitOt <m.mucahit0@gmail.com>
2025-07-14 20:31:13 +0300

    Exercise 9.16 is implemented

--- course_information_typescript/src/components/Content.tsx
+++ course_information_typescript/src/components/Content.tsx
@@ -1,4 +1,5 @@
 import type { CoursePart } from "../data/types";
+import { assertNever } from "../helpers";
 
 export interface ContentProps {
   courseParts: CoursePart[];
@@ -7,9 +8,35 @@ export interface ContentProps {
 const Content = ({ courseParts }: ContentProps) => {
   return (
     <>
-      {courseParts.map((part) => (
-        <p key={part.name}>{part.name} {part.exerciseCount}</p>
-      ))}
+      {courseParts.map((part) => {
+        switch (part.kind) {
+          case "basic":
+            return <div key={part.name} style={{ paddingBottom: "5px" }}>
+              <b>{part.name} {part.exerciseCount}</b>
+              <p>{part.description}</p>
+            </div>
+          case "group":
+            return <div key={part.name} style={{ paddingBottom: "5px" }}>
+              <b>{part.name} {part.exerciseCount}</b>
+              <p>project exercises {part.groupProjectCount}</p>
+            </div>
+          case "background":
+            return <div key={part.name} style={{ paddingBottom: "5px" }}>
+              <b>{part.name} {part.exerciseCount}</b>
+              <p>{part.description}</p>
+              <p>{part.backgroundMaterial}</p>
+            </div>
+          case "special":
+            return <div key={part.name} style={{ paddingBottom: "5px" }}>
+              <b>{part.name} {part.exerciseCount}</b>
+              <p>{part.description}</p>
+              <p>required skills: {part.requirements.join(", ")}</p>
+            </div>
+          default: {
+            assertNever(part);
+          }
+        }
+      })}
     </>
   );
 };
