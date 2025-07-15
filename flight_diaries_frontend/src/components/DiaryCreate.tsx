cf662a3aeae368fbecd367bc79198cde387e109c
mmucahitOt <m.mucahit0@gmail.com>
2025-07-15 19:49:51 +0300

    Exercise 9.20 is implemented

--- flight_diaries_frontend/src/components/DiaryCreate.tsx
+++ flight_diaries_frontend/src/components/DiaryCreate.tsx
@@ -11,24 +11,21 @@ export interface DiaryCreateProps {
 
 const DiaryCreate = ({ fetchDiaries, handleNotification }: DiaryCreateProps) => {
   const [date, setDate] = useState<string>("");
-  const [weather, setWeather] = useState<string>("");
-  const [visibility, setVisibility] = useState<string>("");
+  const [weather, setWeather] = useState<Weather>(Weather.sunny);
+  const [visibility, setVisibility] = useState<Visibility>(Visibility.good);
   const [comment, setComment] = useState<string>("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     diaryService.createDiary({ date, weather: weather as Weather, visibility: visibility as Visibility, comment }).then((data) => {
       if (data && "error" in data) {
-        console.log("data", data);
-        console.log("data.error", data.error);
-        console.log("data.error.message", data.error?.message);
         handleNotification({ message: data.error?.message || "Something went wrong", type: "error" });
       } else {
         handleNotification({ message: "Diary created successfully", type: "success" });
         fetchDiaries();
         setDate("");
-        setWeather("");
-        setVisibility("");
+        setWeather(Weather.sunny);
+        setVisibility(Visibility.good);
         setComment("");
       }
     })
@@ -38,8 +35,22 @@ const DiaryCreate = ({ fetchDiaries, handleNotification }: DiaryCreateProps) => 
     <h2>Add a new entry</h2>
     <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column" }} >
       <input type="date" placeholder="Date" value={date} onChange={(e) => setDate(e.target.value)} />
-      <input type="text" placeholder="Weather" value={weather} onChange={(e) => setWeather(e.target.value)} />
-      <input type="text" placeholder="Visibility" value={visibility} onChange={(e) => setVisibility(e.target.value)} />
+      <div>
+        {Object.values(Weather).map((weather) => (
+          <label key={weather}>
+            <input type="radio" name="weather" value={weather} onChange={(e) => setWeather(e.target.value)} />
+            {weather}
+          </label>
+        ))}
+      </div>
+      <div>
+        {Object.values(Visibility).map((visibility) => (
+          <label key={visibility}>
+            <input type="radio" name="visibility" value={visibility} onChange={(e) => setVisibility(e.target.value)} />
+            {visibility}
+          </label>
+        ))}
+      </div>
       <input type="text" placeholder="comment" value={comment} onChange={(e) => setComment(e.target.value)} />
       <button type="submit">add</button>
     </form>
